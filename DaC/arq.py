2138005c1b10bcf4d62425c024bde92bf7d6eaa1
LeandroAnjosHub <leandro.anjos@sptech.school>
2025-09-03 20:16:21 -0300

    Diagrams pt1

--- DaC/arq.py
+++ DaC/arq.py
@@ -5,59 +5,54 @@ from diagrams.aws.network import InternetGateway, ELB, RouteTable
 from diagrams.aws.compute import EC2
 from diagrams.aws.storage import S3, EFS
 from diagrams.generic.blank import Blank
+from diagrams.custom import Custom
 
-with Diagram("Arq AWS", show=False, direction="TB"):
+with Diagram("Arq AWS", show=False):
 
-    with Cluster("Usuários"):
+    with Cluster(""):
         usuario = User("Usuário")
-        admin = Server("Admin\nSSH")
+        admin = User("Admin\nSSH")
 
     with Cluster("AWS"):
         igw = InternetGateway("Internet Gateway")
 
         with Cluster("VPC"):
             lb = ELB("Load Balancer")
-            rt = RouteTable("Route Table")
-            efs = EFS("EFS")
+            rt = Custom("Route Table", "./route_table.png")
 
             with Cluster("Zona de Disponibilidade 1a"):
-                with Cluster("SubnetPública"):
-                    ec2_pub1 = EC2("EC2 Pública 1a")
-                    grupo_web = Blank("Grupo de Destino Web")
+                with Cluster("Sub-rede Pública"):
+                    ec2_pub1 = Custom("", "./ec2.png")
 
-                with Cluster("SubnetPrivada"):
-                    ec2_priv1 = EC2("EC2 Privada 1a")
+                with Cluster("Sub-rede Privada"):    
+                    ec2_priv1 = Custom("", "./ec2.png")
+
+            efs = EFS("EFS")
 
             with Cluster("Zona de Disponibilidade 1c"):
-                with Cluster("SubnetPública"):
-                    ec2_pub2 = EC2("EC2 Pública 1c")
+                with Cluster("Sub-rede Pública"):
+                    ec2_pub2 = Custom("", "./ec2.png")
+        
 
-                with Cluster("SubnetPrivada"):
-                    ec2_priv2 = EC2("EC2 Privada 1c")
+                with Cluster("Sub-rede Privada"):
+                    ec2_priv2 = Custom("", "./ec2.png")
 
             with Cluster("Zona de Disponibilidade 1d"):
-                with Cluster("SubnetPrivada"):
-                    ec2_priv3 = EC2("EC2 Privada 1d")
+                with Cluster("Sub-rede Privada"):
+                    ec2_priv3 = Custom("", "./ec2.png")
 
     with Cluster("S3 Data Lake"):
         raw = S3("Raw")
         trusted = S3("Trusted")
         curated = S3("Curated")
 
-
-    admin >> ec2_priv1
-    admin >> ec2_priv2
-    admin >> ec2_priv3
-
-    admin >> igw
     usuario >> igw
+    admin >> igw 
 
     igw >> lb
 
     lb >> ec2_pub1
     lb >> ec2_pub2
-
-    grupo_web >> ec2_pub1
 
     ec2_pub1 >> efs
     ec2_pub2 >> efs

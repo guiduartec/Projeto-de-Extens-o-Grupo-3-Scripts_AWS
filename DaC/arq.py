f4ee8eca48064b832bd2a2ab50d1027071507afe
LeandroAnjosHub <leandro.anjos@sptech.school>
2025-09-08 11:48:46 -0300

    Diagrama Pyhton finalizado

--- DaC/arq.py
+++ DaC/arq.py
@@ -7,63 +7,91 @@ from diagrams.aws.storage import S3, EFS
 from diagrams.generic.blank import Blank
 from diagrams.custom import Custom
 
-with Diagram("Arq AWS", show=False):
+with Diagram("Arq AWS", show=False, direction="BT", 
+             graph_attr={"splines": "ortho", "nodesep": "0.25", "ranksep": "3"}):
+
 
     with Cluster(""):
         usuario = User("Usuário")
         admin = User("Admin\nSSH")
 
+    internet = Custom("", "./internet.jpg", width="2", height="2", imagescale="true", fixedsize="true")
+
     with Cluster("AWS"):
+
         igw = InternetGateway("Internet Gateway")
 
         with Cluster("VPC"):
             lb = ELB("Load Balancer")
+
+            
             rt = Custom("Route Table", "./route_table.png")
 
+ 
             with Cluster("Zona de Disponibilidade 1a"):
+                filler = Blank("", width="0.01", height="0.1")
                 with Cluster("Sub-rede Pública"):
+                    filler = Blank("", width="0.01", height="0.1")
+                    security_group = Custom("", "./security_group.png", width="0.7", height="0.7", imagescale="true", fixedsize="true")
                     ec2_pub1 = Custom("", "./ec2.png")
-
-                with Cluster("Sub-rede Privada"):    
+                    ebs = Custom("", "./EBS.png", width="1", height="1", imagescale="true", fixedsize="true")
+                    filler = Blank("", width="0.01")
+                with Cluster("Sub-rede Privada"):
+                    filler = Blank("", width="0.01", height="0.1")
+                    security = Custom("", "./security_group.png", width="0.7", height="0.7", imagescale="true", fixedsize="true")    
                     ec2_priv1 = Custom("", "./ec2.png")
+                    ebs = Custom("", "./EBS.png", width="1", height="1", imagescale="true", fixedsize="true")
+                    filler = Blank("", width="0.01", height="0.1")
+
 
             efs = EFS("EFS")
 
+
             with Cluster("Zona de Disponibilidade 1c"):
+                filler = Blank("", width="0.01", height="0.1")
                 with Cluster("Sub-rede Pública"):
+                    filler = Blank("", width="0.01", height="0.1")
+                    security_group = Custom("", "./security_group.png", width="0.7", height="0.7", imagescale="true", fixedsize="true")
                     ec2_pub2 = Custom("", "./ec2.png")
-        
-
+                    ebs = Custom("", "./EBS.png", width="1", height="1", imagescale="true", fixedsize="true")
+                    filler = Blank("", width="0.01", height="0.1")
                 with Cluster("Sub-rede Privada"):
+                    filler = Blank("", width="0.01", height="0.1")
+                    security_group = Custom("", "./security_group.png", width="0.7", height="0.7", imagescale="true", fixedsize="true")
                     ec2_priv2 = Custom("", "./ec2.png")
+                    ebs = Custom("", "./EBS.png", width="1", height="1", imagescale="true", fixedsize="true")
+                    filler = Blank("", width="0.01", height="0.1")
 
+            
             with Cluster("Zona de Disponibilidade 1d"):
+                filler = Blank("", width="0.01", height="0.1")
                 with Cluster("Sub-rede Privada"):
+                    filler = Blank("", width="0.01", height="0.1")
+                    security_group = Custom("", "./security_group.png", width="0.7", height="0.7", imagescale="true", fixedsize="true")
                     ec2_priv3 = Custom("", "./ec2.png")
+                    bd = Custom("", "./bd.png", width="1", height="1", imagescale="true", fixedsize="true")
+                    filler = Blank("", width="0.01", height="0.1")
 
-    with Cluster("S3 Data Lake"):
-        raw = S3("Raw")
-        trusted = S3("Trusted")
-        curated = S3("Curated")
+        with Cluster("S3 Data Lake"):
+            raw = S3("Raw")
+            trusted = S3("Trusted")
+            curated = S3("Curated")                
 
-    usuario >> igw
-    admin >> igw 
+    usuario >> internet >> igw
+    admin >> internet >> igw 
 
     igw >> lb
 
-    lb >> ec2_pub1
-    lb >> ec2_pub2
+    lb >> [ec2_pub1, ec2_pub2]
 
-    ec2_pub1 >> efs
-    ec2_pub2 >> efs
+    # ec2_pub1 >> efs
+    # ec2_pub2 >> efs
 
-    efs >> ec2_priv1
-    efs >> ec2_priv2
+    rt >> [ec2_pub1, ec2_pub2]
 
-    rt >> ec2_priv1
-    rt >> ec2_priv2
-    rt >> ec2_priv3
+    efs >> [ec2_pub1, ec2_pub2]
 
-    ec2_priv1 >> raw
-    ec2_priv2 >> trusted
-    ec2_priv3 >> curated
+    ec2_priv1 >> ec2_priv3
+    ec2_priv2 >> ec2_priv3     
+
+    ec2_pub1 >> raw

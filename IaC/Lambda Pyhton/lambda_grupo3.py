ea96c79b39a40775a996a339d1fd703700672c62
LeandroAnjosHub <leandro.anjos@sptech.school>
2025-10-07 20:24:27 -0300

    Lambda em Pyhton continuação

--- IaC/Lambda Pyhton/lambda_grupo3.py
+++ IaC/Lambda Pyhton/lambda_grupo3.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 # ======== Configuração AWS =======
 
@@ -13,10 +14,36 @@ s3 = boto3.resource('s3', region_name=regiao)
 
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Lambda grupo 3 funcionando'
-    }
+
+    try:
+        objetos = client.list_objects_v2(Bucket=nome_bucket_raw)
+
+        if 'Contents' not in objetos:
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Nenhum arquivo encontrado no bucket RAW.')
+            }
+    
+        for obj in objetos['Contents']:
+            nome_arquivo = obj['Key']
+            print(f"Lendo arquivo: {nome_arquivo}")
+
+            dados = ler_arquivo_s3(nome_arquivo)
+
+            enviar_para_trusted(nome_arquivo, dados)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Arquivos transferidos com sucesso para o bucket TRUSTED!')
+        }
+
+    except Exception as e:
+        print(f"Erro na execução da Lambda: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Erro: {str(e)}")
+        }
+
 
 def ler_arquivo_s3(fileNameOnBucket):
 
@@ -31,5 +58,18 @@ def ler_arquivo_s3(fileNameOnBucket):
             print("Erro ao ler o arquivo: {e}")    
 
 
-
+def enviar_para_trusted(nome_arquivo, dados):
+    try:
+        if dados is not None:
+            # Grava o arquivo no bucket trusted
+            client.put_object(
+                Bucket=nome_bucket_trusted,
+                Key=nome_arquivo,
+                Body=dados.encode('utf-8')
+            )
+            print(f"Arquivo {nome_arquivo} enviado para o bucket TRUSTED.")
+        else:
+            print(f"Arquivo {nome_arquivo} não possui dados válidos.")
+    except Exception as e:
+        print(f"Erro ao enviar o arquivo {nome_arquivo} para o bucket TRUSTED: {e}")
     
